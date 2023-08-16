cd191a3516df59289abfe4ffba3d6f6b77daa4f5
Kim chaeyeon <gk13578@gmail.com>
2023-08-16 11:43:17 +0900

    resetBtn add

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const arr = [];   // 값을 저장할 array 생성
+const arr = [];     // 랜덤 숫자 3개를 담을 빈 배열 생성
 
-while (arr.length < 3){    // 상수 arr 값의 길이가 3보다 작은동안, 길이가 딱 3이 됐을 때 벗어나기
-    const num = Math.floor(Math.random()*10);     // 상수 num에 숫자 0~9까지의 랜덤한 숫자를 저장
-    if(arr.indexOf(num) === -1){     // 만약 arr의 indexOf 값이 -1 (숫자가 없을 때)이 되었을 때
-        arr.push(num);   // arr에 랜덤숫자 num을 push
+while (arr.length < 3){     // 배열 arr의 길이가 3보다 작거나 같을 때까지
+    const num = Math.floor(Math.random()*10);    // num 배열에 랜덤 0~9까지 숫자를 담고
+    if(arr.indexOf(num) === -1){      // 만약 arr 배열에 랜덤 숫자 num이 없다면
+        arr.push(num);        // 랜덤 숫자를 arr 배열에 추가한다.
     }
 }
-let result = 0;
+let result = 0;    // 정답 시도 함수생성
+
 
 function num(){
-    let num2 = document.getElementById('textResult').value;
-    result++;  // 시도한 값 추가
-    const arr2 = num2.split('');   // 사용자가 입력한 값을 string 배열로 변환
-    let ball = 0;   // 숫자 값
-    let strike = 0;  // 위치 값
-    for(let i = 0; i < 3; i++){   // 3번 돌거야
-        if(arr[i] == arr2[i]){   // 처음 생성 된 랜덤 값의 i번째와 사용자가 입력한 값의 배열 i번째의 값이 일치하는지 확인
-            strike++;           // 일치하면 strike 값을 1 추가
-        } else if(arr.indexOf(Number(arr2[i])) !== -1){   // 만약 아니라면 사용자가 입력한 값의 배열을 number로 변환해주고, 그 값이 arr 배열에 있다면 ball 값을 1 추가
-            ball++;
+    let num2 = document.getElementById('textResult').value;      // html 코드 중 input의 value 값을 num2에 저장
+    result++;      // 정답 시도 +1
+    const arr2 = num2.split('');    // arr2 함수에 num2값을 배열로 변환
+    let ball = 0; 
+    let strike = 0;
+    for(let i = 0; i < 3; i++){
+        if(arr[i] == arr2[i]){   // 처음 뽑은 랜덤 숫자 arr의 i번째와 사용자가 입력한 arr2의 i번째의 숫자가 위치까지 맞을 때 
+            strike++;  // strike +1
+        } else if(arr.indexOf(Number(arr2[i])) !== -1){     // 사용자가 입력한 arr2의 i번째 값을 number 속성으로 변환하고, 처음 뽑은 랜덤 숫자 arr 배열에 있는지 확인해봤을 때 있다면
+            ball++;   // ball +1
         }
     }
-    const main1 = document.getElementsByClassName('mainBox1')[0];
+    const main1 = document.getElementsByClassName('mainBox1')[0];  
     const main2 = document.getElementsByClassName('mainBox2')[0];
 
     const newTag1 = document.createElement('li');
@@ -32,9 +33,17 @@ function num(){
     newTag.innerText = `${ball}B ${strike}S`;
     main2.appendChild(newTag);
 
-    if(strike === 3){     // 위에서 추가한 strike 값이 3이 되었을 때
-        alert(`${result}번만에 성공함ㅊㅊ`);    // 알림창으로 총 시도한 값을 표기, 
-        alert('게임 종료!');  // 게임 종료 선언
+    if(strike === 3){
+        alert(`${result}번 만에 성공함ㅊㅊ`);
+        alert('게임 종료!');
         document.getElementById('reset').value = '다시하기';
     }
+
+    const chk = document.getElementById('reset');
+
+    chk.setAttribute("onClick", "resetBtn()");
+}
+
+function resetBtn(){
+    location.reload();
 }
